efaab01c381c5af55c4f53b7adfd0dd00ff1e6b5
zlqzcc <lz1569@nyu.edu>
2017-10-25 14:02:20 -0400

    baseline fit

--- fitWeibull.m
+++ fitWeibull.m
@@ -2,17 +2,21 @@ dataDir = './NN2006/';
 load(strcat(dataDir, 'SUB1.mat'));
 load(strcat(dataDir, 'SUB2.mat'));
 
-sumLL1 = weibullFit(subject1);
-sumLL2 = weibullFit(subject2);
+[LL1Sub1, LL2Sub1] = weibullFit(subject1);
+[LL1Sub2, LL2Sub2] = weibullFit(subject2);
 
 
-function sumLL = weibullFit(subjectData)
-sumLL = 0; 
+function [sumLL1, sumLL2] = weibullFit(subjectData)
+sumLL1 = 0; 
+sumLL2 = 0;
 
 vlb = [0 0]; vub = [Inf, Inf];
 crtRef  = [0.075, 0.5];
 vRef    = [0.5, 1, 2, 4, 8, 12];
 crtTest = [0.05, 0.075, 0.1, 0.2, 0.4, 0.5, 0.8];
+
+opts = optimset('fminsearch'); opts.Display = 'iter';
+opts.TolX = 1.e-8; opts.MaxFunEvals = 2000;
 
 for i = 1 : length(crtRef)
     refCrst = crtRef(i);
@@ -27,9 +31,10 @@ for i = 1 : length(crtRef)
             objFunc = @(para) weibullCstfunc(data(1, :), data(2, :), para);
             options = optimoptions('fmincon','Diagnostics','off','Display',...
             'iter','Algorithm','interior-point','MaxIter',200);
-            [~, fval, ~, ~] = fmincon(objFunc, 1 + rand(1, 2), [], [], [], [], vlb, vub, [], options);
+            [~, fval1, ~, ~] = fmincon(objFunc, 2 + 0.5 * rand(1, 2), [], [], [], [], vlb, vub, [], options);            
+            [~, fval2, ~, ~] = fminsearchbnd(objFunc, 2 + 0.5 * rand(1, 2), vlb, vub, opts);
             
-            sumLL = sumLL + fval;
+            sumLL1 = sumLL1 + fval1; sumLL2 = sumLL2 + fval2;
         end
     end
 end
