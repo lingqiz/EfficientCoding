71e80ed27d4aded9fc7fc40df7656dcb88273220
zlqzcc <lz1569@nyu.edu>
2019-02-06 15:57:51 -0500

    update plot

--- compareFit.m
+++ compareFit.m
@@ -2,6 +2,7 @@ load('./GaussFitFinal/gauss_final.mat');
 load('./AllFitRes/weibullFitAll.mat');
 
 figure; hold on; grid on;
+set(gca, 'FontSize', 14)
 colors = get(gca,'colororder');
 
 nSub = 5;
@@ -53,7 +54,7 @@ UB = 20; priorSupport = (0.1 : 0.001 : UB);
 if logSpace
     plot(log(priorSupport), log(prior(priorSupport)), style, 'LineWidth', 2);
     
-    labelPos = [0.1, 0.25, 0.5, 1, 2.1 : 2 : 12.1, 20];
+    labelPos = [0.1, 0.25, 0.5, 1, 2.0 : 2 : 12.0, 20];
     xticks(log(labelPos)); 
     xticklabels(arrayfun(@num2str, labelPos, 'UniformOutput', false));
     
