502ac46e083c71a314d36b377fe7e5e52aa4e839
lingqiz <lingqiz@D11566.psych.upenn.edu>
2019-03-01 16:21:52 -0500

    update psych curve

--- plotPsycurves.m
+++ plotPsycurves.m
@@ -7,18 +7,18 @@ load(strcat(dataDir, 'SUB4.mat'));
 load(strcat(dataDir, 'SUB5.mat'));
 
 load('./weibullFitAll.mat');
-load('./GaussFit/gauss_final_0.mat');
+load('./GaussFit/gauss_final_2.mat');
 
-plotPsycurve(subject1, paraSub1, weibullFit1, 'Subject 1:');
+plotPsycurve(subject5, paraSub5, weibullFit5, 'Subject 5:');
 function plotPsycurve(subData, modelPara, weibullPara, titleText)
 
 cRef  = 0.075;
 cTest = [0.05, 0.075, 0.1, 0.2, 0.4, 0.8];
-plotPsycurveCrst(subData, modelPara, weibullPara, cRef, cTest, strcat(titleText, 'Ref Crst = 0.075'))
+plotPsycurveCrst(subData, modelPara, weibullPara, cRef, cTest, strcat(titleText, 'Reference Contrast = 0.075'))
 
 cRef = 0.5;
 cTest = [0.05, 0.1, 0.2, 0.4, 0.5, 0.8];
-plotPsycurveCrst(subData, modelPara, weibullPara, cRef, cTest, strcat(titleText, 'Ref Crst = 0.5'))
+plotPsycurveCrst(subData, modelPara, weibullPara, cRef, cTest, strcat(titleText, 'Reference Contrast = 0.5'))
 
 end
 
@@ -31,42 +31,25 @@ refV       = [0.5, 1, 2, 4, 8, 12];
 supports = [0.25, 1; 0.25, 2; 0.25, 4; 0.25, 10; 0.25, 25; 0.25, 30];
 
 c0 = modelPara(1); c1 = modelPara(2); c2 = modelPara(3);
-noiseLevel = modelPara(4:end); 
-    
+noiseLevel = modelPara(4:end);
+
 % Computing Prior Probability
-domain    = -100 : 0.01 : 100; 
+domain    = -100 : 0.01 : 100;
 priorUnm  = 1.0 ./ ((abs(domain) .^ c0) + c1) + c2;
 nrmConst  = 1.0 / (trapz(domain, priorUnm));
 prior = @(support) (1.0 ./ ((abs(support) .^ c0) + c1) + c2) * nrmConst;
 
 index = reshape(1 : 36, 6, 6)';
-figure; 
+figure('Renderer', 'painters', 'Position', [10 10 1024 768]);
 
 for i = 1 : length(cTest)
-    for j = 1 : length(refV)                
-        crst1 = cRef; v1 = refV(j); crst2 = cTest(i); 
+    for j = 1 : length(refV)
+        crst1 = cRef; v1 = refV(j); crst2 = cTest(i);
         noise1 = noiseLevel(testCrst == crst1);
         noise2 = noiseLevel(testCrst == crst2);
         
         rangeV = supports(refV == v1, :);
         v2 = rangeV(1) : 0.005 : rangeV(2);
-        
-        para = weibullPara(refCrst == crst1, refV == v1, testCrst == crst2, :);
-        pLgrWeibull = wblcdf(v2, para(1), para(2));
-        pLgrBayes = zeros(1, length(v2));
-        
-        [meanRef, stdRef] = efficientEstimator(prior, noise1, v1);
-        for k = 1 : length(pLgrBayes)
-            [meanTest, stdTest] = efficientEstimator(prior, noise2, v2(k));
-            dPrime = (meanTest - meanRef) / sqrt((stdTest ^ 2 + stdRef ^ 2) / 2);
-            pLgrBayes(k) = 0.5 * erfc(-0.5 * dPrime);
-        end
-        
-        subplot(6, 6, index((i-1) * 6 + j));
-        plot(v2, pLgrWeibull, 'r', 'LineWidth', 1.5); hold on;
-        plot(v2, pLgrBayes,   'b', 'LineWidth', 1.5);
-        xlim([rangeV(1) rangeV(2)]); ylim([0, 1]);        
-        title(sprintf('Ref Speed: %.2f \n Test Crst: %.2f', v1, crst2));
         
         testData = subData([3, 9], ...
             subData(2, :) == crst1 & subData(1, :) == v1 & subData(4, :) == crst2);
@@ -77,8 +60,38 @@ for i = 1 : length(cTest)
         for idx = 1:length(testSpeed)
             resProb(idx) = mean(testData(2, idxC' == idx));
             dataCount(idx) = sum(idxC' == idx);
+        end        
+        
+        subplot(6, 6, index((i-1) * 6 + j));
+        if(sum(dataCount) > 0)
+            para = weibullPara(refCrst == crst1, refV == v1, testCrst == crst2, :);
+            pLgrWeibull = wblcdf(v2, para(1), para(2));
+            pLgrBayes = zeros(1, length(v2));
+            
+            [meanRef, stdRef] = efficientEstimator(prior, noise1, v1);
+            for k = 1 : length(pLgrBayes)
+                [meanTest, stdTest] = efficientEstimator(prior, noise2, v2(k));
+                dPrime = (meanTest - meanRef) / sqrt((stdTest ^ 2 + stdRef ^ 2) / 2);
+                pLgrBayes(k) = 0.5 * erfc(-0.5 * dPrime);
+            end
+                        
+            plot(v2, pLgrWeibull, 'r', 'LineWidth', 1.5); hold on;
+            plot(v2, pLgrBayes,   'b', 'LineWidth', 1.5);            
         end
+        
         scatter(testSpeed, resProb, dataCount * scale, 'k');
+        xlim([rangeV(1) rangeV(2)]); ylim([0, 1]);
+        
+        if(i == 1)
+            ylabel(sprintf('Reference Speed:\n%.1f', v1));
+        end        
+        if(j == 1)
+            title(sprintf('Test Contrast:\n%g', crst2), 'FontWeight','Normal');
+        end        
+        if(j == 6)
+            xlabel('Test Speed [deg/sec]');
+        end
+        
     end
 end
 suptitle(titleText);
